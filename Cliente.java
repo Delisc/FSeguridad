1f8692d7eed17bbc130636f9499e38ef6fdd6b9c
Delisc <deliaschz@gmail.com>
2015-06-02 19:08:07 -0500

    Lab 1

--- Cliente.java
+++ Cliente.java
@@ -4,24 +4,34 @@ import java.net.*;
 public class Cliente{
     public static void main(String[] args) {
       String servidor = "127.0.0.1";
+      String cadena;
       int puerto = 5000;
       try{
-        Socket socket= new Socket (servidor,puerto); //crear cliente
-        BufferedReader in = new BufferedReader (new InputStreamReader(socket.getInputStream())); //permite leer entrada de datos
-        PrintWriter out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()),true);
+        Socket socket1= new Socket (servidor,puerto);
+        BufferedReader in = new BufferedReader (new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(socket1.getOutputStream(),true);
+	    DataInputStream	respuesta = new DataInputStream(socket1.getInputStream());
 
-	
-        String c = "Hola!!!";
-        out.println(c);
-	String line = "";
-	while  ((line = in.readLine()) != null){
-		System.out.println(line);
-		break;
-	}
-	socket.close();
-      } catch (IOException e)
-      {
-       		System.out.println("Error en conexión!!!");
+        do{
+            System.out.print("Petición: ");
+            cadena = in.readLine();
+            String cadena1 = "";
+            out.println(cadena);
+            if ((cadena.trim().toUpperCase()).equals("nombre")){
+                cadena1 = "Nombre";                  
+            } else if ((cadena.trim().toUpperCase()).equals("unidad")){
+                cadena1 = "Unidades";                                                        
+            } else if ((cadena.trim().toUpperCase()).equals("usuario")){
+                cadena1 = "Nombre de usuario";
+            }
+            System.out.println(cadena1+" = "+respuesta.readUTF());            
+   	}while(!(cadena.trim().equals("SALIR")));     
+	socket1.close();
+	out.close();
+	in.close();
+	respuesta.close();
+      } catch (IOException e) {
+       	        System.out.println("Error en conexión!!!");
       }
       
     }
--- Servidor.java
+++ Servidor.java
@@ -1,54 +1,64 @@
 import java.net.InetAddress;
 import java.net.*;
 import java.io.*;
+import java.util.*;
+import javax.swing.filechooser.FileSystemView;
 import java.lang.management.*;
 public class Servidor{
    
    public static void main(String args[]){
-		InetAddress pc;
 
 		try {
 
 			int puerto = 5000;
-			ServerSocket sock = new ServerSocket(puerto); //Abrir Socket
-			String comandoSalir = "Exit";
-			String entrada = "";
+			ServerSocket socket= new ServerSocket(puerto); 
+                        String cadena;
 			System.out.println("Servidor iniciado en el puerto " + puerto + "...");
-			while(true){
-				Socket sock1 = sock.accept();       //Espera de la petición
-				System.out.println("Aceptando conexion...");
-				PrintWriter out = new PrintWriter(sock1.getOutputStream(), true);
-				BufferedReader in = new BufferedReader(new InputStreamReader(sock1.getInputStream())); //permite leer entrada de datos
-				pc = InetAddress.getLocalHost();  // Creación de objeto tipo InetAddress
-				
-				
-				while ((entrada = in.readLine()) != null) {					
-				    System.out.println(entrada);
-			    	String pathDesktop = System.getProperty("user.home") + "\\Desktop\\";
-				    out.println("Hola desde el  servidor! -> " + pathDesktop);
-					
-					//String direc = pc.getHostAddress();
-					
+			Socket cliente = socket.accept();
+                        System.out.println("conexion...");
+                        BufferedReader entrada = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
+                        DataOutputStream respuesta = new DataOutputStream(cliente.getOutputStream());
+                        do{
+                        cadena = entrada.readLine();
+                        System.out.println("Peticion:" + cadena);
+                        if ((cadena.trim().toUpperCase()).equals("nombre")){
 
-				   pc = InetAddress.getLocalHost();  // Creación de objeto tipo InetAddress
-				   
-				   System.out.println("PC: "+pc);
-				   System.out.println("Dirección IP : "+pc.getHostAddress());
-				   System.out.println("Usuario: "+pc.getHostName());
-				   System.out.println("Conexión de equipo: "+pc.getCanonicalHostName());
-				  
-				    if (entrada.trim().equals(comandoSalir))
-				        return;
-				} 
-				sock1.close();
-				   
-			}
+                         InetAddress direccionLocal = InetAddress.getLocalHost();
+                                    String nombre = direccionLocal.getHostName();
+                                    System.out.println("Nombre de máquina:" + nombre);
+                                    respuesta.writeUTF(nombre);
+                               
+                                    } else if ((cadena.trim().toUpperCase()).equals("unidad")){
+                                    File unidades[];
+                                    unidades = File.listRoots();
+                                    String Tunidades= "";
 
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.exit(-1);
+                                    for (int i=0;i<unidades.length;i++) {
+                                         String unidad = FileSystemView.getFileSystemView().getSystemDisplayName (unidades[i]);
+                                         String carac = FileSystemView.getFileSystemView().getSystemTypeDescription(unidades[i]);                                        
+                                         System.out.println("["+unidad +" , " +carac+ "]");                                         
+                                         Tunidades = Tunidades + "["+unidad +" , " +carac+ "]";
+                                    }     
+                                    respuesta.writeUTF(Tunidades); 
+
+                                    } else if ((cadena.trim().toUpperCase()).equals("usuario")){
+                                    String nombreU = System.getProperty("user.name");
+                                    System.out.println(nombreU);
+                                    respuesta.writeUTF(nombreU);                                                                        
+                                }					
+                                
+   			        }while(!(cadena.trim().equals("salir")));
+                             entrada.close();
+   			     cliente.close();
+   			     socket.close();
+                             if (cadena.trim().equals("salir"))
+                            return;
+		            } catch (IOException e) {//Error
+			    e.printStackTrace();
+			    System.exit(-1);
 		}
 	}
 
 
 }
+                                  
